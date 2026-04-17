8e87485279f315fe9888b185727709e8d972cb32
Aniket Kumar <pradhananiketkumar@gmail.com>
2026-04-17 23:31:38 +0800

    Enhance TurnOrderStrategy
    Enhanced code documentation for the turn-ordering strategy

--- final_version/strategy/TurnOrderStrategy.java
+++ final_version/strategy/TurnOrderStrategy.java
@@ -3,7 +3,9 @@ package strategy;
 import combatant.Combatant;
 import java.util.*;
 
-public interface TurnOrderStrategy 
-{
+// Strategy to figure out who attacks when
+public interface TurnOrderStrategy {
+
+    // Sorts the combatants into their turn order
     public abstract List<Combatant> order(List<Combatant> list);
 }
